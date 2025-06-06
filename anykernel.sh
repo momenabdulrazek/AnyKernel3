6d6476ad4a36acbc84389493671367f39fa7f3cc
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-06-06 16:09:45 +0300

    Adding Oxygen Banner

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Oxygen Kernel for vayu
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=vayu
-device.name2=
+device.name2=bhima
 device.name3=
 device.name4=
 device.name5=
