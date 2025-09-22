8528e4eb76dbe8949c48547d39cf6ec57d8ea1cd
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-09-22 23:38:51 +0300

    Rebrand to Meow and add A16 support

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Oxygen Kernel for vayu
+kernel.string=Meoww Kernel for vayu
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=bhima
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11 - 15
+supported.versions=11 - 16
 supported.patchlevels=
 '; } # end properties
 
